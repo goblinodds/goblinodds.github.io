8a1d5d30e483611a19b156b84277fbd18f3d588f
goblin <factotumfrankie@gmail.com>
2022-06-27 14:24:01 -0700

    Updates

---
+++ precache-manifest.84d93a9c95e1ce0f418a67d1b434ed37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a72b731353b9d97588bf",
+    "url": "/static/css/main.8c8c59d3.chunk.css"
+  },
+  {
+    "revision": "a72b731353b9d97588bf",
+    "url": "/static/js/main.415e3e86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec9c289eb22a3842187a",
+    "url": "/static/js/2.9b981cee.chunk.js"
+  },
+  {
+    "revision": "e02a220bee90331ce743c62d751c503f",
+    "url": "/static/media/portfolio_BusyBee.e02a220b.png"
+  },
+  {
+    "revision": "0d1c620e704d12520d2147a8bf222b63",
+    "url": "/static/media/portfolio_CSOSummer2022.0d1c620e.png"
+  },
+  {
+    "revision": "8e5e5ba02dd285799c008d9c7945e466",
+    "url": "/static/media/portfolio_CalStateActiveShooter.8e5e5ba0.png"
+  },
+  {
+    "revision": "6cadf49b08910bbf4b82ea8d0592dbe4",
+    "url": "/static/media/portfolio_LegallySpeaking.6cadf49b.png"
+  },
+  {
+    "revision": "dcc36ec0dcc84f55c3e7bdc2b23a1305",
+    "url": "/index.html"
+  }
+];
