85b0b48862e83931cd4066d07cf029d06fc8cabb
goblin <factotumfrankie@gmail.com>
2022-10-04 16:18:34 -0700

    added randomized title to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+const text = ['looking for work', 'learning to code', 'a freelance animator']
 
 function Home() {
+
+    const [index, setIndex] = useState(0);
+
+    // have to use useEffect for setInterval to work
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex(count => (count + 1)); 
+        }, 2000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    console.log(index);
     return <div className='Section'>
-            <h2>goblin is a freelance <span>animator</span> who's learning to <span>code</span>.</h2>
+        {/* % text.length allows it to loop over the array within the bounds of the array */}
+            <h2>goblin is {text[index % text.length]}.</h2>
         </div>
 }
 
 export default Home;
+
